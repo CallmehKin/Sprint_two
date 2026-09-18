0a4a7f023e0c056a303f59ba7e4423be67800c16
CallmehKin <samirahderanian@gmail.com>
2026-09-18 11:40:22 -0300

    Create README.py

---
+++ data/README.py
@@ -0,0 +1,2 @@
+O banco SQLite é criado automaticamente na primeira execução e recebe dados demonstrativos.
+A estrutura foi mantida simples para a Sprint 02 e pode ser migrada para PostgreSQL em uma etapa posterior.
